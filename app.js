0dc8711cd0e97b41a79cfb7d5de9815fc824ca73
Nadine101 <nadinet026@gmail.com>
2022-07-10 21:44:39 -0600

    app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+let name = [ 'Nat','Mat','Pam','James','Nick','Pat','Sandra','Colleen','Kevin','Jada' ];
+let  user_age = [20,30,25,18,35, 16,21,35,40,17];
+
+let first_name = user_age(0);
+let second_name = user_age(1);
+let thrid_name = user_age(2);
+let fourth_name = user_age(3);
+let fifth_name = user_age(4);
+let sixth_name = user_age(5);
+let seventh_name = user_age(6);
+let eight_name = user_age(7);
+let nineth_name = user_age(8);
+let tenth_name = user_age(9);

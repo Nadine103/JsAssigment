e2e9cf7226b1f07bda19cc1acfd26e2080b279ef
Nadine101 <nadinet026@gmail.com>
2022-07-11 20:00:24 -0600

    app.js

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-let name = [ 'Nat','Mat','Pam','James','Nick','Pat','Sandra','Colleen','Kevin','Jada' ];
-let  user_age = [20,30,25,18,35, 16,21,35,40,17];
+// here I add an array with 10 values, adding names to each username.//
+let username = ['Nat','Mat','Pam','James','Nick','Pat','Sandra','Colleen','Kevin','Jada'];
+let  user_age = [18,19,20,21,25,30,35,37,40,41];
 
-let first_name = user_age(0);
-let second_name = user_age(1);
-let thrid_name = user_age(2);
-let fourth_name = user_age(3);
-let fifth_name = user_age(4);
-let sixth_name = user_age(5);
-let seventh_name = user_age(6);
-let eight_name = user_age(7);
-let nineth_name = user_age(8);
-let tenth_name = user_age(9);
+// I have add each user a age that is aleast 18 years and older by writting a while statmente//
+
+let username_count = 0;
+while(username_count > username.length) {
+    console.log(username = [username_count]);
+    console.log(user_age [username_count]);
+    username_count ++;
+}
+ 
+//this is for statement to add each user_age to each username
+for(username_count = 0; username_count < username.length; username_count + 1);
